5008fadff137309288153b91e76a7d84902faea9
Jesús Manuel Fuentes Trejo <futjesus@gmail.com>
2023-11-13 17:30:56 -0500

    feat: first refactor implement kafkajs

--- src/modules/shared/queue/queue.controller.ts
+++ src/modules/shared/queue/queue.controller.ts
@@ -1,58 +1,82 @@
 import {
   Controller,
-  Inject,
   OnModuleDestroy,
   OnModuleInit,
   Post,
 } from '@nestjs/common';
-import {
-  ClientKafka,
-  Ctx,
-  KafkaContext,
-  MessagePattern,
-  Payload,
-} from '@nestjs/microservices';
+import { Consumer, Kafka, Producer } from 'kafkajs';
 import { v4 as uuid } from 'uuid';
 
 import { ConfigService, Configuration } from '../config';
 
 @Controller()
 export class QueueController implements OnModuleDestroy, OnModuleInit {
-  private userTopicName = '';
+  private kafkaClient: Kafka;
+  private kafkaConsumer: Consumer;
+  private kafkaProducer: Producer;
 
-  constructor(
-    @Inject('KAFKA_SERVICE')
-    protected readonly _kafkaClient: ClientKafka,
-    protected readonly _configService: ConfigService,
-  ) {
-    this.userTopicName = this._configService.get(Configuration.USER_TOPIC);
+  constructor(protected readonly configService: ConfigService) {
+    const brokers = configService.get(Configuration.KAFKA_BROKER).split(',');
+    const clientId = configService.get(Configuration.KAFKA_NAME_SERVICE);
+
+    this.kafkaClient = new Kafka({
+      clientId,
+      brokers,
+    });
   }
 
   async onModuleInit() {
-    this._kafkaClient.subscribeToResponseOf(this.userTopicName);
-    await this._kafkaClient.connect();
+    const topic = this.configService.get(Configuration.USER_TOPIC);
+    const groupId = this.configService.get(Configuration.KAFKA_NAME_CONSUMER);
+    this.kafkaConsumer = this.kafkaClient.consumer({
+      groupId: `${groupId}-${process.pid}`,
+    });
+    this.kafkaProducer = this.kafkaClient.producer();
+
+    await this.kafkaConsumer.connect();
+    await this.kafkaProducer.connect();
+
+    await this.kafkaConsumer.subscribe({
+      topic,
+      fromBeginning: true,
+    });
+
+    await this.kafkaConsumer.run({
+      eachMessage: async ({ topic, message }) => {
+        const testTopic = this.configService.get(Configuration.USER_TOPIC);
+        const mapper = {
+          [testTopic]: this.testMethod,
+        };
+
+        mapper[topic](JSON.parse(message.value.toString()));
+      },
+    });
   }
 
   async onModuleDestroy() {
-    await this._kafkaClient.close();
+    this.kafkaConsumer.disconnect();
+    this.kafkaProducer.disconnect();
   }
 
   @Post('message-kafka')
   async createMessage() {
-    this._kafkaClient.emit(this.userTopicName, {
-      key: process.pid,
-      value: JSON.stringify({
-        id: uuid(),
-        name: 'Jesús Manuel',
-        lastName: 'Fuentes Trejo',
-      }),
+    const topic = this.configService.get(Configuration.USER_TOPIC);
+    this.kafkaProducer.send({
+      topic,
+      messages: [
+        {
+          key: String(process.pid),
+          value: JSON.stringify({
+            id: uuid(),
+            name: 'Jesús Manuel',
+            lastName: 'Fuentes Trejo',
+          }),
+        },
+      ],
     });
   }
 
-  @MessagePattern('test.example')
-  testExample(@Payload() message: any, @Ctx() context: KafkaContext) {
-    const originalMessage = context.getMessage();
-    console.log(originalMessage.value);
-    return originalMessage.value;
+  private testMethod(message: any) {
+    console.log(message);
   }
 }
--- src/modules/shared/queue/queue.module.ts
+++ src/modules/shared/queue/queue.module.ts
@@ -1,42 +1,10 @@
 import { Module } from '@nestjs/common';
-import { ClientsModule, Transport } from '@nestjs/microservices';
 
-import { ConfigModule, ConfigService, Configuration } from '../config';
+import { ConfigModule } from '../config';
 import { QueueController } from './queue.controller';
 
 @Module({
-  imports: [
-    ConfigModule,
-    ClientsModule.registerAsync([
-      {
-        name: 'KAFKA_SERVICE',
-        inject: [ConfigService],
-        useFactory: async (configService: ConfigService) => {
-          const clientId = configService.get(Configuration.KAFKA_NAME_SERVICE);
-          const groupId = configService.get(Configuration.KAFKA_NAME_CONSUMER);
-          const brokers = configService
-            .get(Configuration.KAFKA_BROKER)
-            .split(',');
-
-          return {
-            transport: Transport.KAFKA,
-            options: {
-              client: {
-                clientId,
-                brokers,
-              },
-              consumer: {
-                groupId: `${groupId}-${process.pid}`,
-              },
-              subscribe: {
-                fromBeginning: true,
-              },
-            },
-          };
-        },
-      },
-    ]),
-  ],
+  imports: [ConfigModule],
   controllers: [QueueController],
 })
 export class QueueModule {}
